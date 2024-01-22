94b06138bd2d5447dab7a7086f3f00ddec12da40
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2024-01-22 13:26:54 +0530

    completed login api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,17 @@
+""" Django Import """
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
-from rest_framework import viewsets, filters 
-from profiles_api import models 
+from rest_framework import viewsets, filters
 from rest_framework.authentication import TokenAuthentication 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings 
+
+""" Import from Profile Part """
+from profiles_api import serializers
+from profiles_api import models 
 from profiles_api import permissions
+
 
 
 
@@ -105,3 +111,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile, )
     filter_backends = (filters.SearchFilter, )  # Corrected the typo from 'filter.SearchFilter' to 'filters.SearchFilter'
     search_fields = ('name', 'email', )
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
